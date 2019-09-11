ef50d96537b89a6277aa79ba28e930a9727f3c53
João Pedro Pagno <pagno.jp@gmail.com>
2019-09-11 11:44:13 -0300

    Updates

---
+++ precache-manifest.aa07c7dbe8e7a51c914a52315a061a27.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445f80cd2bfb397ac4a6f5b836923f59",
+    "url": "/app02-gitcompare/index.html"
+  },
+  {
+    "revision": "d9696ce7a60759a30b3d",
+    "url": "/app02-gitcompare/static/css/2.9a8e69ff.chunk.css"
+  },
+  {
+    "revision": "d9696ce7a60759a30b3d",
+    "url": "/app02-gitcompare/static/js/2.c19bbddc.chunk.js"
+  },
+  {
+    "revision": "0107944700180aeae4c8",
+    "url": "/app02-gitcompare/static/js/main.4f21cbbd.chunk.js"
+  },
+  {
+    "revision": "a6f1ebcbf01d94500de4",
+    "url": "/app02-gitcompare/static/js/runtime~main.487b221e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/app02-gitcompare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/app02-gitcompare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/app02-gitcompare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/app02-gitcompare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/app02-gitcompare/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/app02-gitcompare/precache-manifest.aa07c7dbe8e7a51c914a52315a061a27.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app02-gitcompare/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
